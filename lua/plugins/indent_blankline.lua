0a21046f310de11f2409db1b3d3de8d12f403463
MinhNguyen <vu.nguyen@beesolutions.com>
2023-06-06 15:52:35 +0700

    xxxx

---
+++ lua/plugins/indent_blankline.lua
@@ -0,0 +1,38 @@
+local M = {}
+-- local indent = require("indent_blankline")
+local indent = require("hlchunk")
+M.setup = function()
+  indent.setup {
+  --   space_char_blankline = " ",
+  --   show_current_context = true,
+  --   show_current_context_start = true,
+    chunk = {
+      enable = true,
+      use_treesitter = true,
+      chars = {
+          horizontal_line = "►",
+          vertical_line = "│",
+          left_top = "╭",
+          left_bottom = "╰",
+          right_arrow = "►",
+      },
+      style = {
+          { fg = "#3b3935" },
+      },
+    },
+
+    indent = {
+      enable = false,
+    },
+
+    line_num = {
+      enable = false,
+    },
+
+    blank = {
+      enable = false,
+    },
+  }
+end
+
+return M
